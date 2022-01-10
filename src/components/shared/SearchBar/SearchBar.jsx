c09d4dd3b37d79ace1846cb2010b08d891ea2898
Marina20V <meri20vit@gmail.com>
2022-01-10 15:40:46 +0200

    feat: update search for SearcBar

--- src/components/shared/SearchBar/SearchBar.jsx
+++ src/components/shared/SearchBar/SearchBar.jsx
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import s from './SearchBar.module.scss';
 
 export default function SearchBar() {
+    const[filteredData, setFilteredData] = useState([]);
+    const [events, setEvent] = useState([]);
+    const [wordEntered, setWordEntered] = useState(['']);
+
+
+ 
+    const getEvents = async () => {
+        const response = await axios.get('http://localhost:5000/events');
+        setEvent(response.data);
+    }
+ 
+    useEffect(() => {
+        getEvents();
+    }, []);
+ 
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord)
+        const newFilter = events.filter((value) => {
+            return value.title.includes(searchWord) 
+        });
+        setFilteredData(newFilter);
+    }
+
+    const clearInput = () => {
+        setFilteredData([])
+    }
+
     return (
+        <>
         <div className="header">
             <form>
                 <div className={s.form_box}>
                 <i className="fa fa-search"></i>
                     <input type="text" 
                            className={`${s.search_field} ${s.events_search}`} 
-                           placeholder="Concert, Webinar, Conferences, etc." />
+                           placeholder="Webinars, Startup, Concert, etc." vaue={wordEntered}  onChange={handleFilter}/>
                            <i className="fa fa-search-location"></i>
                         <input type="text" 
                            className={`${s.search_field} ${s.location}`} 
-                           placeholder="City" />
-                            <button className={s.search_btn} type="button">Search</button>
+                           placeholder="(City" />
+                                       {filteredData.length === 0 ?(
+                        <button className={s.search_btn} type="button">Search</button>
+
+                    ) : (<button className={s.clearSearch_btn} onClick={clearInput} type="button">Search</button>)
+                    }
                 </div>
             </form>
+            {filteredData.length !==0 &&(
+            <div className={s.dataResult}>
+                {events.map((value) => {
+                    return (
+                        <div className={s.dataResult} key={value.id}>
+                            {value.title}
+                            </div>
+                    )
+                })}
+            </div>
+
+            )}
             
         </div>
+        </>
     )
 }
