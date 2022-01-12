284a82a90beac8f8e9368116ebb6a22ac35a75ff
Marina20V <meri20vit@gmail.com>
2022-01-12 15:10:27 +0200

    feat(eventitem): fix scss for event

--- src/components/EventItem/EventItem.jsx
+++ src/components/EventItem/EventItem.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import s from './EventItem.module.scss';
-import event111 from '../../assets/images/slider/even111.jpeg' 
+// import event111 from '../../assets/images/slider/even111.jpeg' 
 
 function EventItem(props) {
-
-
-    // const url = window.URL || window.webkitURL;
-
-    // const convertImg = (img) =>{
-    //     const convertToBase = btoa(img);
-    //     const base64Icon = `data:image/png;base64,${convertToBase}`;
-    //     const urlConvert = url.createObjectURL(base64Icon);
-   
-    //     // console.log(urlConvert);
-    //     console.log(urlConvert);
-    //     return urlConvert;
-    // }
-
-
-
-
     /* eslint-disable react/prop-types */
     return (
         
@@ -31,7 +14,7 @@ function EventItem(props) {
                 <figure className={s.events__item__pic} data-category={props.label}>
                     {/* <img src={btoa(props.src)} alt="events-card" className={s.events__item__img}/> */}
 
-                    <img src={event111} alt="events-card" className={s.events__item__img}/>
+                    <img src={props.img_url} alt="events-card" className={s.events__item__img}/>
                     {/* {console.log('data:image/bmp;base64,'+ Base64.encode(props.src))} */}
 
 
@@ -39,15 +22,17 @@ function EventItem(props) {
                 </figure>
                 <div className={s.events__item__info} >
                     <h6 className={s.events__item__text}>{props.text}</h6>
-                    <div className={s.events_list}>
+                    {/* <div className={s.events_list}> */}
                     <ul className={s.ul_events_list}>
                     <li className={s.events__item__date}>{props.date}</li>
                     <li className={s.events__item__location}>{props.location}</li>
                     <li className={s.events__item__entry}>{props.entry}</li>
                     <li className={s.events__item__type}>{props.type}</li>
                     </ul>
-                    </div>
+                    {/* </div> */}
+                     <div className={s.el_details}>   
                     <details className={s.events__item__desc}>{props.desc}</details>
+                    </div>
                 </div>
             </Link>
         </li>
