ace3179730232bc1919ba6d55420bc6e20a3405b
Marina20V <meri20vit@gmail.com>
2022-01-13 12:38:23 +0200

    feat: creating new cpmponent AddItem form

--- src/components/AddItem/AddItemForm.jsx
+++ src/components/AddItem/AddItemForm.jsx
@@ -1,113 +1,128 @@
-//  import React from "react";
-//  import './AddItem.scss';
-// //  import 'bootstrap/dist/css/bootstrap.min.css';
-// //  import "../node_modules/bootstrap/scss/bootstrap.scss";
+import axios from 'axios';
+import React, {useState} from 'react';
+// import {useNavigate}  from 'react-router-dom';
+// import FileUpload from '../shared/FileUpload/FileUpload';
+import s from './AddItem.module.scss';
+
+export default function AddItemForm() {
+    // const navigate = useNavigate();
+            /* eslint-disable no-unused-vars */ 
+
+    const [title, setTitle] = useState("");
+    const [fullDesc, setDescription] = useState("");
+    const [location, setLocation] = useState("");
+    const [date, setDate] = useState("");
+    const [category, setCategory] = useState("");
+    const [organizerId, setOrganizer] = useState("");
+    const [subcatType, setType] = useState("");
+    const [subcatEntry, setEntry] = useState("");
+    const [website, setWebsite] = useState("");
+    const [file, setFile] = useState(null);
 
 
-//  export default function AddItemForm() {
-//    return (
-//      <div>
-//        <div className="container">
-//          <section className="panel panel-default">
-//            <div className="panel-heading">
-//            <h3 className="panel-title">Panel heading</h3> 
-//            </div>
-//            <div className="panel-body">
-//            { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//            <form action="designer-finish.html" className="form-horizontal" htmlFor="form" id="form" >
-//                <div className="form-group">
-//                { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//  	<label htmlFor="name" className="col-sm-3 control-label">Тип заказа</label>
-//          <div className="col-sm-9">
-//          { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//  		<label className="radio-inline">
-//          <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/> Внешный заказ 
-//  	</label>
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label className="radio-inline">
-//  	  <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/> Внутренный заказ 
-//  	</label>
-//      </div>
-//      </div>
-//      <div className="form-group">
-//      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="name" className="col-sm-3 control-label">Буюртмачи</label>
-//      <div className="col-sm-9">
-//        <input type="text" className="form-control" name="name" id="name" placeholder="Исм ёки корхона номи" />
-//        </div>
-//      </div>
-//      <div className="form-group">
-//      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="name" className="col-sm-3 control-label">Название продукта</label>
-//      <div className="col-sm-9">
-//        <input type="text" className="form-control" name="name" id="name" placeholder="Название" />
-//      </div>
-//      </div>
-//      <div className="form-group">
-//      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="about" className="col-sm-3 control-label">Описание</label>
-//      <div className="col-sm-9">
-//        <textarea className="form-control"></textarea>
-//      </div>
-//    </div>
 
-//    <div className="form-group">
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="qty" className="col-sm-3 control-label">Количество</label>
-//      <div className="col-sm-3">
-//     <input type="text" className="form-control" name="qty" id="qty" placeholder="шт."/>
-//      </div>
-//    </div>
-//    <div className="form-group">
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//    <label className="col-sm-3 control-label">Сроки</label>
-//    <div className="col-sm-3"> 
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//    <label className="control-label small" htmlFor="date_start">Начало: </label>
-//  	  <input type="text" className="form-control" name="date_start" id="date_start" placeholder="Начало"/>
-//    </div>
-//    <div className="col-sm-3">   
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//  	  <label className="control-label small" htmlFor="date_finish">Конец:</label>
-//  	  <input type="text" className="form-control" name="date_finish" id="date_finish" placeholder="Конец" />
-//      </div>
-//        </div>
-//        <div className="form-group">
-//        { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="name" className="col-sm-3 control-label">Загрузить</label>
-//      <div className="col-sm-3">
-//      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//        <label className="control-label small" htmlFor="file_img">Картинка (jpg/png):</label> <input type="file" name="file_img"/>
-//      </div>
-//      <div className="col-sm-3">
-//      { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//        <label className="control-label small" htmlFor="file_img">Файлы:</label>  <input type="file" name="file_archive" />
-//      </div>
-//    </div>
+    
+    const saveProduct = async (e) => {   
+        e.preventDefault();
+        
+        
+    const formData = new FormData();
+    formData.append('title', title)
+    formData.append('fullDesc', fullDesc)
+    formData.append('location', location)
+    formData.append('date', date)
+    formData.append('category', category)
+    formData.append('organizerId', organizerId)
+    formData.append('subcatType', subcatType)
+    formData.append('subcatEntry', subcatEntry)
+    formData.append('website', website)
+    formData.append('file', file)
 
-//    <div className="form-group">
-//    { /* eslint-disable-next-line jsx-a11y/label-has-associated-control */ }
-//      <label htmlFor="tech" className="col-sm-3 control-label">Технолог</label>
-//      <div className="col-sm-3">
-//     <select className="form-control">
-//  	<option value="">Выберите</option>
-//  	<option value="texnolog2">Технолог 2</option>
-//  	<option value="texnolog3">Технолог 3</option>
-//     </select>
-//      </div>
-//    </div>
+        console.log('nckej');
 
-//    <hr/>
-//    <div className="form-group">
-//      <div className="col-sm-offset-3 col-sm-9">
-//        <button type="submit" className="btn btn-primary">Отправить</button>
-//      </div>
-//    </div>
-//    </form>
+        await axios({
+            method: 'post',
+            url: 'http://localhost:5000/events',
+            data: formData,
+            headers: {'Content-Type': 'multipart/form-data'}
+            });
+        // navigate(`/`);
+    }
+ 
+    return (
+            
+        <form onSubmit={saveProduct} className={s.createEventForm}>
+            <input
+              name="title"
+              placeholder="title"
+              type="text"
+              id="title"
+              onChange={(e=>setTitle(e.target.value))}
+            />
+            <input
+              name="full_desc"
+              placeholder="description"
+              type="text"
+              id="full_desc"
+              onChange={(e=>setDescription(e.target.value))}
+            />
+            <input
+              name="location"
+              placeholder="location"
+              type="text"
+              id="location"
+              onChange={(e=>setLocation(e.target.value))}
+            />
+             <input
+              name="date"
+              placeholder="date"
+              type="text"
+              id="date"
+              onChange={(e=>setDate(e.target.value))}
+            />
+            <select placeholder="Category" onChange={(e=>setCategory(e.target.value))}>
+             <option>ALL</option>
+             <option>BUSINESS</option>
+             <option>ART</option>
+             <option>EDUCATION</option>
+             <option>FESTIVAL</option>
+             <option>FOOD</option>
+             <option>NIGHTLIFE</option>
+             <option>SPORTS</option>
+             <option>IT\&TECHNOLOGIES</option>
+            </select>
 
-//                  </div>
-//          </section>
-//        </div>
-//      </div>
-//    );
-//  }
+            {/* <FileUpload /> */}
+            <input
+              name="organizer name"
+              placeholder="organizer name"
+              type="text"
+              id="organizer name"
+              onChange={(e=>setOrganizer(e.target.value))}
+            />
+            <select onChange={(e=>setType(e.target.value))} placeholder="Type">
+             <option>ONLINE</option>
+             <option>OFFLINE</option>
+            </select>
+            <select placeholder="Entry" onChange={(e=>setEntry(e.target.value))}>
+             <option>FREE</option>
+             <option>PAID</option>
+            </select>
+            <input
+              name="website"
+              placeholder="Website"
+              type="text"
+              id="website"
+              onChange={(e=>setWebsite(e.target.value))}
+            />
+                        <input
+              name="file"
+              placeholder="file"
+              type="file"
+              id="file"
+              onChange={e=>setFile(e.target.files[0])}
+            />
+          <button type="submit" >Save event</button>
+        </form>
+    )
+}
