1dda233a2cc05b2ba1de1d3a4fc4fb4d29cbe163
Marina20V <meri20vit@gmail.com>
2022-01-15 02:26:10 +0200

    perf: make footer responsive

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -7,99 +7,80 @@ import constants from "../../../constants/constants";
 
 export default function Footer() {
   return (
-    <div className={s.main_footer_container}>
-      <footer className={s.footer}>
-        <div className={s.footer_container}>
-          <div className={s.footer_row}>
-            <div className={s.footer_col}>
-              <h4>{constants.FRESHEST_NEWS}</h4>
-              <p className={s.footer_subscription_text}>
-                {constants.UNSUBSCRIBE}
-              </p>
-              <div className={s.input_areas}>
-                <form action="">
-                  <input
-                    type="email"
-                    name="email"
-                    placehoklder="Your email"
-                    className={s.footer_input}
-                  />
-                  <Button buttonStyle={s.btn__putline}>{constants.SUBSCRIBE}</Button>
-                </form>
-              </div>
-            </div>
-            <div className={s.footer_col}>
-              <h4>{constants.CONTACT_US}</h4>
-              <ul className={s.footer_list}>
-                <li>
-                  <a href="/">
-                    <i className="fas fa-map-marker-alt" /> {constants.EVENTS_LOCATION}{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
-                    <i className="fas fa-envelope" /> {constants.EVENTS_SITE}{" "}
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
-                    <i className="fas fa-phone-alt" /> {constants.EVENTS_PHONE}{" "}
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className={s.footer_col}>
-              <h4>{constants.FOLLOW_US}</h4>
-              <div className={s.social_links}>
-                <a href="/event">
-                  <i
-                    role="button"
-                    aria-label="Facebook"
-                    className="fab fa-facebook-f"
-                  ></i>
-                </a>
-                <a href="/event">
-                  <i
-                    role="button"
-                    aria-label="Twitter"
-                    className="fab fa-twitter"
-                  ></i>
-                </a>
-                <a href="/event">
-                  <i
-                    role="button"
-                    aria-label="Instagram"
-                    className="fab fa-instagram"
-                  ></i>
-                </a>
-                <a href="/event">
-                  <i
-                    role="button"
-                    aria-label="Linkedin"
-                    className="fab fa-linkedin-in"
-                  ></i>
-                </a>
-              </div>
-            </div>
+    <footer className={s.footer}>
+      <div className={s.footer_row}>
+        <div className={s.footer_col}>
+          <h4>{constants.FRESHEST_NEWS}</h4>
+          <p className={s.footer_subscription_text}>{constants.UNSUBSCRIBE}</p>
+          <form className={s.input_areas} action="">
+            <input
+              type="email"
+              name="email"
+              placehoklder="Your email"
+              className={s.footer_input}
+            />
+            <Button buttonStyle={s.btn__putline}>{constants.SUBSCRIBE}</Button>
+          </form>
+        </div>
+        <div className={s.footer_col}>
+          <h4>{constants.CONTACT_US}</h4>
+          <ul className={s.footer_list}>
+            <li>
+              <a href="/">
+                <i className="fas fa-map-marker-alt" />{" "}
+                {constants.EVENTS_LOCATION}{" "}
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="fas fa-envelope" /> {constants.EVENTS_SITE}{" "}
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <i className="fas fa-phone-alt" /> {constants.EVENTS_PHONE}{" "}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={s.footer_col}>
+          <h4>{constants.FOLLOW_US}</h4>
+          <div className={s.social_links}>
+            <a href="/event">
+              <i
+                role="button"
+                aria-label="Facebook"
+                className="fab fa-facebook-f"
+              ></i>
+            </a>
+            <a href="/event">
+              <i
+                role="button"
+                aria-label="Twitter"
+                className="fab fa-twitter"
+              ></i>
+            </a>
+            <a href="/event">
+              <i
+                role="button"
+                aria-label="Instagram"
+                className="fab fa-instagram"
+              ></i>
+            </a>
+            <a href="/event">
+              <i
+                role="button"
+                aria-label="Linkedin"
+                className="fab fa-linkedin-in"
+              ></i>
+            </a>
           </div>
         </div>
-      <section className={s.socia_media}>
-        <div className={s.social_media_wrap}>
-            <ul className={s.footer_end} >
-          <li className={s.footer_logo}>
-            <Link to="/" className={s.social_logo}>
-              <img src={logo} alt="Logo" />
-            </Link>
-          </li>
-          <li>
-          <small className={s.website_rights}>
-            {constants.RIGHTS}
-          </small>
-          </li>
-          </ul>
-          </div>
-          </section>
-      </footer>
-    </div>
+      </div>
+        <Link to="/" className={s.social_logo}>
+          <img src={logo} alt="Logo" />
+        </Link>
+        <small className={s.website_rights}>{constants.RIGHTS}</small>
+    </footer>
   );
 }
